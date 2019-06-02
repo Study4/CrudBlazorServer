342d560538b702d8ad9355ed452caae2b1d32a5b
Sky Chang <skchang@microsoft.com>
2019-06-02 20:05:01 +0800

    move try cache to ui layer

--- CrudBlazor.FrontServerSite/Service/EmployeestService.cs
+++ CrudBlazor.FrontServerSite/Service/EmployeestService.cs
@@ -21,90 +21,44 @@ namespace CrudBlazor.FrontServerSite.Service
         public async Task<IEnumerable<Employee>> GetAsync()
         {
             var _httpClient = _clientFactory.CreateClient("backendApi");
-            try
-            {
-                var result = await _httpClient.GetAsync("api/Employees/");
-                result.EnsureSuccessStatusCode();
-                return await result.Content.ReadAsAsync<IEnumerable<Employee>>();
-
-
-            }
-            catch (Exception ex)
-            {
-
-                throw ex;
-            }
+            var result = await _httpClient.GetAsync("api/Employees/");
+            result.EnsureSuccessStatusCode();
+            return await result.Content.ReadAsAsync<IEnumerable<Employee>>();
         }
 
         public async Task<Employee> GetAsync(int id)
         {
             var _httpClient = _clientFactory.CreateClient("backendApi");
-            try
-            {
-                var result = await _httpClient.GetAsync($"api/Employees/{id}");
-                result.EnsureSuccessStatusCode();
-                return await result.Content.ReadAsAsync<Employee>();
-
-
-            }
-            catch (Exception ex)
-            {
-
-                throw ex;
-            }
+            var result = await _httpClient.GetAsync($"api/Employees/{id}");
+            result.EnsureSuccessStatusCode();
+            return await result.Content.ReadAsAsync<Employee>();
         }
 
         public async Task<Employee> Add(Employee employee)
         {
             var _httpClient = _clientFactory.CreateClient("backendApi");
-            try
-            {
-                var result = await _httpClient
-                    .PostAsync<Employee>($"api/Employees/", employee, new JsonMediaTypeFormatter());
-                result.EnsureSuccessStatusCode();
-                return await result.Content.ReadAsAsync<Employee>();
-                    
-
-            }
-            catch (Exception ex)
-            {
-
-                throw ex;
-            }
+            var result = await _httpClient
+                .PostAsync<Employee>($"api/Employees/", employee, new JsonMediaTypeFormatter());
+            result.EnsureSuccessStatusCode();
+            return await result.Content.ReadAsAsync<Employee>();
         }
 
         public async Task<bool> Update(int id, Employee employee)
         {
             var _httpClient = _clientFactory.CreateClient("backendApi");
-            try
-            {
-                var result = await _httpClient
-                    .PutAsync<Employee>($"api/Employees/{id}", employee, new JsonMediaTypeFormatter());
-                result.EnsureSuccessStatusCode();
-                return true;
-            }
-            catch (Exception ex)
-            {
-
-                throw ex;
-            }
+            var result = await _httpClient
+                .PutAsync<Employee>($"api/Employees/{id}", employee, new JsonMediaTypeFormatter());
+            result.EnsureSuccessStatusCode();
+            return true;
         }
 
         public async Task<bool> Del(int id)
         {
             var _httpClient = _clientFactory.CreateClient("backendApi");
-            try
-            {
-                var result = await _httpClient
-                    .DeleteAsync($"api/Employees/{id}");
-                result.EnsureSuccessStatusCode();
-                return true;
-            }
-            catch (Exception ex)
-            {
-
-                throw ex;
-            }
+            var result = await _httpClient
+                .DeleteAsync($"api/Employees/{id}");
+            result.EnsureSuccessStatusCode();
+            return true;
         }
     }
 }
